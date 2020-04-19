cb92d49560b0b188b3f7e80514b60bd659b55643
Jeff Davey <jeffdavey@submersion.com>
2020-04-19 00:40:30 -0600

    Add spawning

---
+++ Timer.gd
@@ -0,0 +1,22 @@
+extends Timer
+
+
+onready var Food = preload("res://Entities/Food/Food.tscn")
+
+
+func _on_Timer_timeout():
+	var rand = RandomNumberGenerator.new()
+	rand.randomize()
+
+	var screen_size = get_viewport().get_visible_rect().size
+	var food = Food.instance()
+	
+	food.calories = rand.randi_range(0, 1000)
+	food.tileId = rand.randi_range(0, 63)
+
+	var x = rand.randf_range(0,screen_size.x)
+	var y = rand.randf_range(0,screen_size.y)
+	
+	food.position.x = x
+	food.position.y = y
+	add_child(food)
