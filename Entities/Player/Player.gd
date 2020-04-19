83d08afaac903041519eb6149cd9501c1c0a3c28
Jeff Davey <jeffdavey@submersion.com>
2020-04-18 19:28:50 -0600

    Move left down animation speed

--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -1,11 +1,13 @@
 extends KinematicBody2D
 
-
+export(int, 1, 5) var fatLevel = 1
 export var speed = 500
 export var acceleration = 2500
 export var friction = 5000
+export var animationSpeed = 0
 
 var velocity: Vector2
+onready var animations = $Sprite
 
 func _physics_process(delta):
 	var direction: Vector2
@@ -18,3 +20,21 @@ func _physics_process(delta):
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	move_and_collide(velocity * delta)
+	
+	var animation = get_animation_direction(direction)
+	animationSpeed = 2 + 13 * direction.length()
+	animations.frames.set_animation_speed(animation, animationSpeed)
+	animations.play(animation)
+
+
+func get_animation_direction(direction: Vector2):
+	var norm_direction = direction.normalized()
+	var animation = str(fatLevel) + "-down"
+	if norm_direction.y <= -0.707:
+		animation = str(fatLevel) + "-up"
+	elif norm_direction.x <= -0.707:
+		animation = str(fatLevel) +  "-left"
+	elif norm_direction.x >= 0.707:
+		animation = str(fatLevel) + "-right"
+	return animation
+	
