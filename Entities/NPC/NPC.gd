72366ecd39e6e620c553d1a580ff247d5775b587
Jeff Davey <jeffdavey@submersion.com>
2020-04-19 19:48:47 -0600

    Setup shader to change pance

--- Entities/NPC/NPC.gd
+++ Entities/NPC/NPC.gd
@@ -1,14 +1,36 @@
 extends KinematicBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var colorShader
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var changeColor = """
+shader_type canvas_item;
+
+uniform vec3 pants_color = vec3(1, .2, .3);
+const vec4 base_pants = vec4(0, 0.6, .85882, 1);
+
+void fragment() {
+	COLOR = texture(TEXTURE,UV); // Get current color of pixel
+
+	vec4 diff = COLOR - base_pants;
+	if (abs(length(diff)) < 0.01) {
+		COLOR.rgb = pants_color
+	}
+}"""
+
+	var colorShader = Shader.new()
+	colorShader.code = changeColor
+	var materialShader = ShaderMaterial.new()
+	materialShader.shader = colorShader
+	var sprite = get_node("Sprite")	
+	sprite.material = materialShader
+	
+	
+	
+	
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Entities/Player/Player.gd
+++ Entities/Player/Player.gd
@@ -31,8 +31,10 @@ var fatLevels = [
 var state = State.MOVING
 
 func _ready():
+	var sprite = self.get_node("Sprite")
 	var _err = self.get_node("Area2D").connect("area_entered", self, "_on_Area2D_area_entered")
-	_err = self.get_node("Sprite").connect("animation_finished", self, "_on_Sprite_animation_finished")
+	_err = sprite.connect("animation_finished", self, "_on_Sprite_animation_finished")
+	#sprite.material.set_shader_param("pants_color", Vector3(1, 0, 0))
 
 func _input(event):
 	if event.is_action_pressed("ToggleFat"):
