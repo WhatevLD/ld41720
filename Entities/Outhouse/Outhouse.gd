c73138678886c6ffa711f9fed4fd4c71a15501c5
Nodlehs <johnsk16@gmail.com>
2020-04-20 00:10:42 -0500

    Added in outhouse sounds effects

--- Entities/Outhouse/Outhouse.gd
+++ Entities/Outhouse/Outhouse.gd
@@ -5,6 +5,7 @@ export var poopTime = 5
 onready var animations = $AnimatedSprite
 signal done_pooping
 var poopTimer
+var poopSoundTimer
 
 func _ready():
 	add_to_group("outhouse")
@@ -13,12 +14,23 @@ func _ready():
 	poopTimer.wait_time = poopTime
 	poopTimer.connect("timeout", self, "on_poop_complete")
 	add_child(poopTimer)
+	poopSoundTimer = Timer.new()
+	poopSoundTimer.one_shot = true
+	poopSoundTimer.wait_time = 2
+	poopSoundTimer.connect("timeout", self, "on_soundTimer_complete")
+	add_child(poopSoundTimer)
+
+func on_soundTimer_complete():
+	$Poop.play()
 
 func on_poop_complete():
 	animations.play("open")
+	$Door.play()
 	emit_signal("done_pooping")
 
 func poop(time):	
 	animations.play("closed")
 	poopTimer.wait_time = time
+	$Door.play()
 	poopTimer.start()
+	poopSoundTimer.start()
